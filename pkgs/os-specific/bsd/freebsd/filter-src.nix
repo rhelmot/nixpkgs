b4b9e450a3b74d4437ddbb5e5fe2ec5b0ef24805
Artemis Tosini <me@artem.ist>
2024-02-21 05:23:39 +0000

    freebsd.filterSource: copy symlinks, clean up code

--- pkgs/os-specific/bsd/freebsd/filter-src.nix
+++ pkgs/os-specific/bsd/freebsd/filter-src.nix
@@ -1,13 +1,11 @@
-{lib, runCommand, pkgsBuildBuild, source, ...}: {pname, path, extraPaths ? []}: runCommand "${pname}-filtered-src" {
+{ lib, pkgsBuildBuild, runCommand, writeText, source }:
+{ pname, path, extraPaths ? [] }:
+let
+  sortedPaths = lib.naturalSort ([ path ] ++ extraPaths);
+  filterText = writeText "${pname}-src-include"
+    (lib.concatMapStringsSep "\n" (path: "/${path}") sortedPaths);
+in runCommand "${pname}-filtered-src" {
   nativeBuildInputs = [ (pkgsBuildBuild.rsync.override { enableZstd = false; enableXXHash = false; }) ];
 } ''
-  for p in ${lib.concatStringsSep " " ([ path ] ++ extraPaths)}; do
-    set -x
-    path="$out/$p"
-    mkdir -p "$(dirname "$path")"
-    src_path="${source}/$p"
-    if [[ -d "$src_path" ]]; then src_path+=/; fi
-    rsync --chmod="+w" -r "$src_path" "$path"
-    set +x
-  done
+  rsync -a -r --files-from=${filterText} ${source}/ $out
 ''
