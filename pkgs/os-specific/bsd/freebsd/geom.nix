29075b7a76d9f0c36ab2696e125394847171300e
Artemis Tosini <me@artem.ist>
2024-02-29 20:48:21 +0000

    freebsd.geom: build class libraries for commands

--- pkgs/os-specific/bsd/freebsd/geom.nix
+++ pkgs/os-specific/bsd/freebsd/geom.nix
@@ -1,4 +1,43 @@
-{ mkDerivation, libbsdxml, libgeom, libsbuf }:
+{
+  mkDerivation,
+  libbsdxml,
+  libgeom,
+  libsbuf,
+  libufs,
+  openssl,
+}:
+let
+  libs = mkDerivation {
+    name = "geom-class-libs";
+    path = "lib/geom";
+    extraPaths = [
+      "lib/Makefile.inc"
+      "sbin/geom"
+      "sys/geom"
+
+      # geli isn't okay with just libcrypt, it wants files in here
+      "sys/crypto/sha2"
+      "sys/opencrypto"
+    ];
+
+    # libgeom needs sbuf and bsdxml but linker doesn't know that
+    NIX_LDFLAGS = "-lbsdxml -lsbuf";
+    buildInputs = [
+      libbsdxml
+      libgeom
+      libsbuf
+      libufs
+      openssl
+    ];
+
+    # tools want geom headers but don't seem to declare it
+    preBuild = ''
+      export NIX_CFLAGS_COMPILE="-I$BSDSRCDIR/sys $NIX_CFLAGS_COMPILE";
+    '';
+
+    clangFixup = true;
+  };
+in
 mkDerivation {
   path = "sbin/geom";
   extraPaths = [
@@ -6,9 +45,14 @@ mkDerivation {
     "lib/geom"
   ];
 
-  # libgeom needs sbuf and bsdxml but linker doesn't know that
+  GEOM_CLASS_DIR = "${libs}/lib";
+
   NIX_LDFLAGS = "-lbsdxml -lsbuf";
-  buildInputs = [ libbsdxml libgeom libsbuf ];
+  buildInputs = [
+    libbsdxml
+    libgeom
+    libsbuf
+  ];
 
   clangFixup = true;
 }
