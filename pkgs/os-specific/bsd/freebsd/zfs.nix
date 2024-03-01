afb8a228a66c9f55c5fdde0a93262cb4b57c9ea4
Artemis Tosini <me@artem.ist>
2024-03-01 22:20:35 +0000

    freebsd.zfs: also build zpool

--- pkgs/os-specific/bsd/freebsd/zfs.nix
+++ pkgs/os-specific/bsd/freebsd/zfs.nix
@@ -6,13 +6,15 @@
   libspl,
   libzfs,
   openssl,
+  zfs-data,
 }:
 mkDerivation {
   path = "cddl/sbin/zfs";
   extraPaths = [
+    "cddl/compat/opensolaris"
+    "cddl/sbin/zpool"
     "sys/contrib/openzfs"
     "sys/modules/zfs"
-    "cddl/compat/opensolaris"
   ];
 
   buildInputs = [
@@ -22,6 +24,16 @@ mkDerivation {
     libzfs
     openssl
   ];
+
+  postPatch = ''
+    sed -i 's|/usr/share/zfs|${zfs-data}/share/zfs|' $BSDSRCDIR/cddl/sbin/zpool/Makefile
+  '';
+
+  # I lied, this is both zpool and zfs
+  preBuild = ''
+    make -C $BSDSRCDIR/cddl/sbin/zpool $makeFlags
+    make -C $BSDSRCDIR/cddl/sbin/zpool $makeFlags install
+  '';
 
   clangFixup = true;
 
