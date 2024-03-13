0fb0b66e7fc5836f686e3426a3e82114834b226d
Artemis Tosini <me@artem.ist>
2024-03-13 22:45:41 +0000

    freebsd.sys: move source patching outside of derivation

--- pkgs/os-specific/bsd/freebsd/by-name/sys.nix
+++ pkgs/os-specific/bsd/freebsd/by-name/sys.nix
@@ -1,80 +1,105 @@
-{mkDerivation, buildPackages, buildFreebsd, hostArchBsd, patchesRoot, ... }:
-mkDerivation (let
-    cfg = "GENERIC";
-  in rec {
+{ lib, mkDerivation, buildPackages, buildFreebsd, hostArchBsd, patchesRoot
+, filterSource, applyPatches
+, baseConfig ? "GENERIC"
+, extraFlags ? { }
+}:
+let
+  filteredSource = filterSource {
+    pname = "sys";
     path = "sys";
-    extraPaths = ["include"];
-
-    nativeBuildInputs = (with buildPackages; [
-      bsdSetupHook
-      mandoc
-      groff
-      gawk
-      #ctfconvert
-    ]) ++ (with buildFreebsd; [
-      freebsdSetupHook
-      bmakeMinimal
-      install
-      config
-      rpcgen
-      file2c
-      bintrans
-      xargs-j
-    ]);
-
+    extraPaths = [ "include" ];
+  };
+  patchedSource = applyPatches {
+    src = filteredSource;
     patches = [
       /${patchesRoot}/sys-gnu-date.patch
       /${patchesRoot}/sys-no-explicit-intrinsics-dep.patch
     ];
-
-    # --dynamic-linker /red/herring is used when building the kernel.
-    NIX_ENFORCE_PURITY = 0;
-
-    AWK = "${buildPackages.gawk}/bin/awk";
-
-    CWARNEXTRA = "-Wno-error=shift-negative-value -Wno-address-of-packed-member";
-
-    MK_CTF = "no";
-    hardeningDisable = [
-      "pic"  # generates relocations the linker can't handle
-      "stackprotector"  # generates stack protection for the function generating the stack canary
-    ];
-
-    # hardeningDisable = stackprotector doesn't seem to be enough, put it in cflags too
-    NIX_CFLAGS_COMPILE = "-fno-stack-protector";
-
-    KODIR = "${builtins.placeholder "out"}/kernel";
-    KMODDIR = "${builtins.placeholder "out"}/kernel";
-    DTBDIR = "${builtins.placeholder"out"}/dbt";
-
-    KERN_DEBUGDIR = "${builtins.placeholder "debug"}/lib/debug";
-    KERN_DEBUGDIR_KODIR = "${KERN_DEBUGDIR}/kernel";
-    KERN_DEBUGDIR_KMODDIR = "${KERN_DEBUGDIR}/kernel";
-
-    skipIncludesPhase = true;
-
-    configurePhase = ''
-      runHook preConfigure
-
-      for f in conf/kmod.mk contrib/dev/acpica/acpica_prep.sh; do
+    postPatch = ''
+      for f in sys/conf/kmod.mk sys/contrib/dev/acpica/acpica_prep.sh; do
         substituteInPlace "$f" --replace 'xargs -J' 'xargs-j '
       done
 
-      for f in conf/*.mk; do
+      for f in sys/conf/*.mk; do
         substituteInPlace "$f" --replace 'KERN_DEBUGDIR}''${' 'KERN_DEBUGDIR_'
       done
 
-      cd ${hostArchBsd}/conf
-      sed -i ${cfg} \
+      sed -i sys/${hostArchBsd}/conf/${baseConfig} \
         -e 's/WITH_CTF=1/WITH_CTF=0/' \
         -e '/KDTRACE/d'
-      config ${cfg}
-
-      runHook postConfigure
     '';
-    preBuild = ''
-      cd ../compile/${cfg}
-    '';
+  };
+in mkDerivation rec {
+  pname = "sys";
 
-    outputs = ["out" "debug"];
-  })
+  # Patch source outside of this derivation so out-of-tree modules can use it
+  src = patchedSource;
+  COMPONENT_PATH = "sys";
+
+  nativeBuildInputs = (with buildPackages; [
+    bsdSetupHook
+    mandoc
+    groff
+    gawk
+  ]) ++ (with buildFreebsd; [
+    freebsdSetupHook
+    bmakeMinimal
+    install
+    config
+    rpcgen
+    file2c
+    bintrans
+    xargs-j
+  ]);
+
+  # --dynamic-linker /red/herring is used when building the kernel.
+  NIX_ENFORCE_PURITY = 0;
+
+  AWK = "${buildPackages.gawk}/bin/awk";
+
+  CWARNEXTRA = "-Wno-error=shift-negative-value -Wno-address-of-packed-member";
+
+  hardeningDisable = [
+    "pic"  # generates relocations the linker can't handle
+    "stackprotector"  # generates stack protection for the function generating the stack canary
+  ];
+
+  # hardeningDisable = stackprotector doesn't seem to be enough, put it in cflags too
+  NIX_CFLAGS_COMPILE = "-fno-stack-protector";
+
+  env = {
+    MK_CTF = "no";
+  } // (lib.flip lib.mapAttrs' extraFlags (name: value: {
+    name = "MK_${lib.toUpper name}";
+    value = if value then "yes" else "no";
+  }));
+
+  KODIR = "${builtins.placeholder "out"}/kernel";
+  KMODDIR = "${builtins.placeholder "out"}/kernel";
+  DTBDIR = "${builtins.placeholder"out"}/dbt";
+
+  KERN_DEBUGDIR = "${builtins.placeholder "debug"}/lib/debug";
+  KERN_DEBUGDIR_KODIR = "${KERN_DEBUGDIR}/kernel";
+  KERN_DEBUGDIR_KMODDIR = "${KERN_DEBUGDIR}/kernel";
+
+  skipIncludesPhase = true;
+
+  configurePhase = ''
+    runHook preConfigure
+
+    cd ${hostArchBsd}/conf
+    config ${baseConfig}
+
+    runHook postConfigure
+  '';
+  preBuild = ''
+    cd ../compile/${baseConfig}
+  '';
+
+  outputs = ["out" "debug"];
+
+  meta = {
+    description = "FreeBSD kernel and modules";
+    platforms = lib.platforms.freebsd;
+  };
+}
