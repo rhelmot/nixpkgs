24821aa88ab64a9bd4df5731d9a9e1d9729facb7
Artemis Tosini <me@artem.ist>
2024-03-14 05:21:08 +0000

    freebsd.kldxref: Follow links

--- pkgs/os-specific/bsd/freebsd/by-name/kldxref.nix
+++ pkgs/os-specific/bsd/freebsd/by-name/kldxref.nix
@@ -1,4 +1,9 @@
 { mkDerivation, ... }:
 mkDerivation {
   path = "usr.sbin/kldxref";
+
+  # We symlink in our modules, make it follow symlinks
+  postPatch = ''
+    sed -i 's/FTS_PHYSICAL/FTS_LOGICAL/' $BSDSRCDIR/usr.sbin/kldxref/kldxref.c
+  '';
 }
