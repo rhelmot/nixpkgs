e33c7eede7d0f5e9c9e2c6d5893b214a088b4f27
Audrey Dutcher <audrey@rhelmot.io>
2024-07-24 16:34:42 -0700

    update bootstrap hashes

--- pkgs/stdenv/freebsd/bootstrap-files/x86_64-unknown-freebsd.nix
+++ pkgs/stdenv/freebsd/bootstrap-files/x86_64-unknown-freebsd.nix
@@ -1,13 +1,13 @@
 {
   unpack = import <nix/fetchurl.nix> {
-    url = "http://192.168.122.1:8000/result/on-server/unpack.nar.xz";
-    hash = "sha256-y6quCU9JKnKBdHDcUkdkM0ypWDT2cdSiqR1WqA+8ozE=";
+    url = "https://s3.us-east-005.backblazeb2.com/nixbsd/kvmqx0askgdy4yb9hkd6mdp5wxylhdzf-unpack.nar.xz";
+    hash = "sha256-03Jp/wqXY1he1eVF5uAdGVrOTTg+d+NPfJZWgi8HG+Q=";
     name = "boostrapUnpacked";
     unpack = true;
   };
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = "http://192.168.122.1:8000/result/on-server/bootstrap-tools.tar.xz";
-    hash = "sha256-ypIOxsB8a/RPupki0ZTjb+vuE+ibtmS8e3DazeolHj8=";
+    url = "https://s3.us-east-005.backblazeb2.com/nixbsd/hxbyy4fc6gxx41v8212g2m3h6v9awb6p-bootstrap-tools.tar.xz";
+    hash = "sha256-tUuBuUZhBK2OFAzzRpFGNFsMscvLMW9TN4Vy8VT0aw8=";
     name = "bootstrapTools.tar.xz";
   };
 }
