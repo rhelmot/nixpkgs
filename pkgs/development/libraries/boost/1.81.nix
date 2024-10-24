f4894478021870dcade3dbc1b8680536c9a42aa1
Audrey Dutcher <audrey@rhelmot.io>
2024-10-23 23:07:58 -0700

    boost 1.81: backport some patches for openbsd's sake

--- pkgs/development/libraries/boost/1.81.nix
+++ pkgs/development/libraries/boost/1.81.nix
@@ -1,4 +1,4 @@
-{ callPackage, fetchurl, ... } @ args:
+{ callPackage, fetchurl, fetchpatch, ... } @ args:
 
 callPackage ./generic.nix (args // rec {
   version = "1.81.0";
@@ -11,4 +11,20 @@ callPackage ./generic.nix (args // rec {
     # SHA256 from http://www.boost.org/users/history/version_1_81_0.html
     sha256 = "71feeed900fbccca04a3b4f2f84a7c217186f28a940ed8b7ed4725986baf99fa";
   };
+
+  # patches backported from 1.86
+  patches = [
+    (fetchpatch {
+      name = "openbsd-fiber-futex.patch";
+      url = "https://github.com/boostorg/fiber/pull/315/commits/d486740347c3d2c2116f9f1e50623936c00e7889.patch";
+      hash = "sha256-upDPbHhfJfV6s4W4R2kW5PaMtyxKmvNDVWmTuwlkp9U=";
+      relative = "include";
+    })
+    (fetchpatch {
+      name = "openbsd-atomic-futex.patch";
+      url = "https://github.com/boostorg/atomic/commit/6ee31382b271193064db2bfe0a446260c5b344c0.patch";
+      hash = "sha256-8MKIK9UjVKTlGAkDCpj6Y/PdwoYLTPktNSxSNuCOueg=";
+      relative = "include";
+    })
+   ];
 })
