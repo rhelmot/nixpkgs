c0390a018176524d3fcd78ce26cc5de82c05e345
Audrey Dutcher <audrey@rhelmot.io>
2024-10-31 18:46:27 -0700

    in my bootloader. straight up debugging it. and by 'it'. haha. well lets jusr say. my filesystem stack

--- pkgs/os-specific/bsd/openbsd/pkgs/stand/package.nix
+++ pkgs/os-specific/bsd/openbsd/pkgs/stand/package.nix
@@ -9,6 +9,7 @@ mkDerivation {
     find $BSDSRCDIR -name Makefile -print0 | xargs -0 sed -E -i -e 's/-nopie/-no-pie/g'
     substituteInPlace $BSDSRCDIR/sys/arch/*/stand/boot/check-boot.pl --replace-fail /usr/bin/objdump objdump
     substituteInPlace $BSDSRCDIR/sys/arch/*/stand/Makefile --replace-quiet " boot " " " --replace-quiet " fdboot " " "
+    substituteInPlace $BSDSRCDIR/sys/arch/amd64/stand/efiboot/conf.c --replace-fail 'debug = 0' 'debug = 1'
   '';
 
   # expects to be able to use unprefixed programs
@@ -23,6 +24,7 @@ mkDerivation {
     ln -s ${pkgsBuildTarget.binutils}/bin/${pkgsBuildTarget.binutils.targetPrefix}objcopy $TMP/bin/objcopy
     ln -s ${pkgsBuildTarget.binutils}/bin/${pkgsBuildTarget.binutils.targetPrefix}objcopy $TMP/bin/
 
-    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -Wno-pointer-sign"
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -Wno-pointer-sign -DEFI_DEBUG -DBIOS_DEBUG -DCOMPAT_UFS "
+    NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -DDEBUG" make $makeFlags -C $BSDSRCDIR/sys/arch/amd64/stand/efiboot/bootx64 dev_i386.o
   '';
 }
