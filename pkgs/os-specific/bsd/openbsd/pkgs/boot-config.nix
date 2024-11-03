96bb11ad956585b4540ba318414ed5f75f3c622d
Artemis Tosini <me@artem.ist>
2024-11-03 22:44:32 +0000

    openbsd.boot-config: Use openbsd.compat

--- pkgs/os-specific/bsd/openbsd/pkgs/boot-config.nix
+++ pkgs/os-specific/bsd/openbsd/pkgs/boot-config.nix
@@ -1,8 +1,8 @@
-{ mkDerivation, lib, flex, byacc }:
+{ mkDerivation, lib, flex, byacc, compatHook }:
 mkDerivation {
   path = "usr.sbin/config";
 
-  extraNativeBuildInputs = [ flex byacc ];
+  extraNativeBuildInputs = [ flex byacc compatHook ];
 
   postPatch = ''
     rm $BSDSRCDIR/usr.sbin/config/ukc.c
@@ -12,16 +12,6 @@ mkDerivation {
   '';
 
   buildPhase = ''
-    mkdir $TMP/include
-    mkdir $TMP/include/sys
-
-    echo '#include_next <sys/types.h>' >>$TMP/include/sys/types.h
-    echo '#define O_EXLOCK 0' >>$TMP/include/sys/types.h
-    echo '#include <sys/sysmacros.h>' >>$TMP/include/sys/types.h
-    echo '#define pledge(a, b) 0' >>$TMP/include/sys/types.h
-    echo '#define errc(a, b, c, ...) (void)0' >>$TMP/include/sys/types.h
-
-
     for f in *.l; do flex $f; done
     for f in *.y; do yacc -H ''${f%.y}.h $f; done
     for f in *.c; do $CC -I$TMP/include -DMAKE_BOOTSTRAP -c $f; done
