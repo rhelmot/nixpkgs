15c6fc7cb43622cf3ba42338fc72e4953003505f
Artemis Tosini <me@artem.ist>
2024-11-03 22:45:29 +0000

    openbsd.boot-ctags: use openbsd.compat

--- pkgs/os-specific/bsd/openbsd/pkgs/boot-ctags.nix
+++ pkgs/os-specific/bsd/openbsd/pkgs/boot-ctags.nix
@@ -1,18 +1,10 @@
-{ mkDerivation, lib, flex, byacc }:
+{ mkDerivation, lib, flex, byacc, compatHook }:
 mkDerivation {
   path = "usr.bin/ctags";
 
-  extraNativeBuildInputs = [ flex byacc ];
+  extraNativeBuildInputs = [ flex byacc compatHook ];
 
   buildPhase = ''
-    mkdir $TMP/include
-    mkdir $TMP/include/sys
-
-    echo '#include_next <sys/types.h>' >>$TMP/include/sys/types.h
-    echo '#define pledge(a, b) 0' >>$TMP/include/sys/types.h
-
-    echo '#include <dirent.h>' >>$TMP/include/sys/dirent.h
-
     for f in *.l; do flex $f; done
     for f in *.y; do yacc -H ''${f%.y}.h $f; done
     for f in *.c; do $CC -I$TMP/include -DMAKE_BOOTSTRAP -c $f; done
